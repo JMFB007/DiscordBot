6084e06f97acd167cf7a65d131378c7f6a6200ac
José Manuel <62104898+JMFB007@users.noreply.github.com>
2022-04-22 05:01:23 +0000

    test...?

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
+<<<<<<< HEAD
 import discord
 from discord.ext import commands, tasks
+=======
+from replit import db
+
+>>>>>>> origin/main
 from StaynAlive import StaynAlive
 
 bot = commands.Bot(command_prefix = "-")#, help_command=None)
 
+<<<<<<< HEAD
 @bot.event
 async def on_ready():
   print("Logged in as {0.user}".format(bot))
@@ -13,6 +19,18 @@ async def on_ready():
 @tasks.loop(seconds=10)
 async def change_status():
   await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="your orders!"))
+=======
+my_secret = os.environ['TOKEN']
+client = discord.Client()
+creep = ["creeper","Creeper"]
+pog = ["Pog","pog","Poggers","poggers"]
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  msg = message.content
+>>>>>>> origin/main
 
 SLUM = os.environ['SLUM']
     
@@ -36,6 +54,7 @@ async def unload(ctx, extension):
     except:
       await ctx.send(f"{extension} is already unloaded or doesnt exist")
     else:
+<<<<<<< HEAD
       await ctx.send(f"{extension} was unloaded")
   else:
     await ctx.send(f"{ctx.author.id}, you cant use this command")
@@ -71,3 +90,29 @@ for filename in os.listdir("./cogs"):
 BotToken = os.environ['TOKEN']
 StaynAlive()
 bot.run(BotToken)
+=======
+      n=4
+      await message.channel.send(other.insult() + at)
+      while n>0:
+        time.sleep(10)
+        await message.channel.send(other.insult() + at)
+        n-=1
+  if msg.startswith("$add "):
+    txt = msg.split("$add ",1)[1]
+    db["aa"] = txt
+    await message.channel.send(f"{txt} was added")
+  if msg.startswith("$show"):
+    txt = ""
+    for key in db.keys():
+      txt += key + "   "
+    await message.channel.send(txt)
+  if msg.startswith("$delete"):
+    for key in db.keys():
+      txt = db[key]
+      del db[key]
+      await message.channel.send(f"{txt} was deleted")
+  if any(word in msg for word in creep):
+    await message.channel.send("AW MAN")
+  if any(word in msg for word in pog):
+    await message.channel.send("https://tenor.com/view/poggers-pepe-gif-12187647")
+>>>>>>> origin/main
